875ed292ef282ffeded2d77f329505714b018e78
yurchykyur <yurchyk.yur.s@gmail.com>
2023-05-16 14:33:33 +0300

    was used prettier

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
 // Завдання 1 - галерея зображень
-// Створи галерею з можливістю кліку по її елементах 
-// і перегляду повнорозмірного зображення у модальному вікні. 
+// Створи галерею з можливістю кліку по її елементах
+// і перегляду повнорозмірного зображення у модальному вікні.
 // Подивися демо відео роботи галереї.
 
 // Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
@@ -10,30 +10,28 @@
 // 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr  https://www.jsdelivr.com/package/npm/basiclightbox
 //    і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
 // 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
+// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям.
 // 6. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
-
 // Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, 
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>,
 // і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
 
-//  Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. 
+//  Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку.
 //  Заборони цю поведінку за замовчуванням.
-
 
 // Закриття з клавіатури
 // УВАГА
 // Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
 
-// Додай закриття модального вікна після натискання клавіші Escape. 
-// Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. 
+// Додай закриття модального вікна після натискання клавіші Escape.
+// Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно.
 // Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
 
 import { galleryItems } from './gallery-items.js';
 
 const refs = {
-    gallery: document.querySelector('.gallery'),
+  gallery: document.querySelector('.gallery'),
 };
 
 const instance = basicLightbox.create(`
@@ -47,32 +45,34 @@ refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarksup);
 refs.gallery.addEventListener('click', onGalleryItemClick);
 
 /**
- * tracks a click on a gallery image and triggers the Window Keydown tracking function, 
+ * tracks a click on a gallery image and triggers the Window Keydown tracking function,
  * the vendor launch function whose parameter is href
- * @param {Click} event 
+ * @param {Click} event
  * @returns stops execution of the function if the condition is not met
  */
-function onGalleryItemClick(event){
-    event.preventDefault();
-    const isGalleryItemElemImage = event.target.classList.contains('gallery__image');
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  const isGalleryItemElemImage =
+    event.target.classList.contains('gallery__image');
 
-    if (!isGalleryItemElemImage) {
-    return
-    };
+  if (!isGalleryItemElemImage) {
+    return;
+  }
 
-    addtListenWindowKeydown();
+  addtListenWindowKeydown();
 
-    openImageInBasicLightbox(event.target.closest('.gallery__link').href);
-};
+  openImageInBasicLightbox(event.target.closest('.gallery__link').href);
+}
 
 /**
  * creation of html markup of gallery elements, in the form of a string
- * @param {Object} galleryItems 
+ * @param {Object} galleryItems
  * @returns html markup of gallery elements, in the form of a string
  */
 function createGalleryItemsMarksup(galleryItems) {
-return galleryItems.map(({preview, original, description}) => {
-return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
 <li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -83,16 +83,16 @@ return `
     />
   </a>
 </li>`;
-}).join('');
-
-};
+    })
+    .join('');
+}
 
 /**
  * opens a large image from the gallery
  * @param {String} src link to large image
  */
 function openImageInBasicLightbox(src) {
-  console.log(instance.element().querySelector('img').src = src);
+  console.log((instance.element().querySelector('img').src = src));
   instance.show();
 }
 
@@ -100,27 +100,25 @@ function openImageInBasicLightbox(src) {
  * add EventListener on window by keydown
  */
 function addtListenWindowKeydown() {
-    window.addEventListener('keydown', onEscKeyDown)
+  window.addEventListener('keydown', onEscKeyDown);
 }
 
 /**
- * tracks whether a button is pressed 'Escape' 
- * and runs functions to delete the block, which was created by basiclightbox, 
+ * tracks whether a button is pressed 'Escape'
+ * and runs functions to delete the block, which was created by basiclightbox,
  * stop tracking the keydown event
- * @param {keydown} e 
+ * @param {keydown} e
  */
 function onEscKeyDown(e) {
- if (e.code === 'Escape') {
+  if (e.code === 'Escape') {
     removeListenWindowKeydown();
     instance.close();
- };
-};
+  }
+}
 
 /**
  * remove EventListener on window by keydown
  */
 function removeListenWindowKeydown() {
- window.removeEventListener('keydown', onEscKeyDown)
-};
-
-
+  window.removeEventListener('keydown', onEscKeyDown);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,8 +4,8 @@
 
 // Завдання 2 - бібліотека SimpleLightbox
 
-// Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox, 
-// яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна, 
+// Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox,
+// яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна,
 // а також гортання зображень за допомогою клавіатури.
 
 // Необхідно трохи змінити розмітку картки галереї, використовуй цей шаблон.
@@ -18,39 +18,39 @@
 
 // Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
 
-// ++ Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. 
+// ++ Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 // ++ Використовуй готовий код з першого завдання.
-// ++ Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. 
+// ++ Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs.
 // ++ Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
-// ++ Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. 
+// ++ Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery.
 // ++ Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
-// ++ Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. 
+// ++ Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt.
 // Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
 
 import { galleryItems } from './gallery-items.js';
 
-
 const refs = {
-    gallery: document.querySelector('.gallery'),
+  gallery: document.querySelector('.gallery'),
 };
 
 const galleryItemsMarksup = createGalleryItemsMarksup(galleryItems);
 
 refs.gallery.insertAdjacentHTML('beforeend', galleryItemsMarksup);
 
-const lightbox = new SimpleLightbox('.gallery a', { 
-    captionsData:	'alt',
-    captionDelay: 250,
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 
 /**
  * creation of html markup of gallery elements, in the form of a string
- * @param {Object} galleryItems 
+ * @param {Object} galleryItems
  * @returns html markup of gallery elements, in the form of a string
  */
 function createGalleryItemsMarksup(galleryItems) {
-return galleryItems.map(({preview, original, description}) => {
-return `
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
 <li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -60,6 +60,6 @@ return `
     />
   </a>
 </li>`;
-}).join('');
+    })
+    .join('');
 }
-
